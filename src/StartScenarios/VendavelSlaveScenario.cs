a293c4313690200528a396da9f10a92c192252d1
IggyTheMad <77555981+IggyTheMads@users.noreply.github.com>
2022-02-18 02:24:03 -0600

    Merged Vendavel Scenario

--- src/StartScenarios/VendavelSlaveScenario.cs
+++ src/StartScenarios/VendavelSlaveScenario.cs
@@ -12,41 +12,51 @@ namespace AlternateStart.StartScenarios
     {
         public override Scenarios Type => Scenarios.VendavelSlave;
         public override ScenarioDifficulty Difficulty => ScenarioDifficulty.Hard;
-        public override ScenarioTheme Theme => ScenarioTheme.Freeform;
         public override ScenarioAreas Area => ScenarioAreas.Chersonese;
 
         public override AreaManager.AreaEnum SpawnScene => AreaManager.AreaEnum.ChersoDungeon1;
         public override Vector3 SpawnPosition => new(-4.9f, -10f, 26.9f);
         public override Vector3 SpawnRotation => new(0, 356.3f, 0);
 
-        public override string SL_Quest_FileName => null;
-        public override int SL_Quest_ItemID => -1;
+        public override bool HasQuest => false;
+        public override string QuestName => "";
+        public override Dictionary<string, string> QuestLogSignatures => new()
+        {
+
+        };
+
 
         //variables
         public string enemyID = "com.iggy.vendavelbandit";
         public Vector3 prisonJump = new(43f,-9.7f, 34.7f);
         public Vector3 prisonDoors = new(15f, -10f, 4.4f);
 
-
-        public override void PreScenarioBegin()
+        public override void OnScenarioChosen()
         {
-
+            VanillaQuestsHelper.StartHouseTimer();
         }
-        public override void OnScenarioBegin()
+
+        public override void OnScenarioChosen(Character character)
         {
+            character.Stats.IncreaseBurntHealth(200, 1);
+            //character.Inventory.ReceiveItemReward(3000133, 1, true); //beggarB head
+            //character.Inventory.ReceiveItemReward(3000130, 1, true); //beggarB chest
+            //character.Inventory.ReceiveItemReward(3000136, 1, true); //beggarB legs
+        }
+
+        public override void OnStartSpawn()
+        {
+
             VanillaQuestsHelper.StartHouseTimer();
 
             var myChar = SL.GetSLPack("iggythemad AlternateStart").CharacterTemplates[enemyID];
             myChar.Spawn(prisonJump, Vector3.back, UID.Generate());
+
         }
 
         public override void OnStartSpawn(Character character)
         {    
              character.Stats.IncreaseBurntHealth(200, 1);
-             /*character.Inventory.ReceiveItemReward(3000133, 1, true); //beggarB head
-             character.Inventory.ReceiveItemReward(3000130, 1, true); //beggarB chest
-             character.Inventory.ReceiveItemReward(3000136, 1, true); //beggarB legs*/
-             character.Inventory.RemoveMoney(27, true);
         }
 
         public override void UpdateQuestProgress(Quest quest)
